34cf676f9bea6b2b73ac8bde832fffc23f3dbe58
Juanmas-hub <juancitobravoc@gmail.com>
2025-07-31 04:49:16 +0000

    feat: add user struct

--- user-service/internal/model/user_model.go
+++ user-service/internal/model/user_model.go
@@ -1,1 +1,15 @@
 package model
+
+import(
+	"time"
+)
+
+type User struct {
+    ID        int       `json:"id" db:"id"`
+    Username  string    `json:"username" db:"username"`
+    Email     string    `json:"email" db:"email"`
+    Password  string    `json:"password" db:"password"`
+    ChatIDs   []int     `json:"chat_ids" db:"chat_ids"`
+    CreatedAt time.Time `json:"created_at" db:"created_at"`
+    UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
