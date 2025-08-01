f63141a4295b7d14a35bd202ce56970dc3112014
Juanmas-hub <juancitobravoc@gmail.com>
2025-08-01 06:39:44 +0000

    refactor: add auxiliar functions

--- user-service/controllers/usersController.go
+++ user-service/controllers/usersController.go
@@ -2,113 +2,60 @@ package controllers
 
 import (
 	"net/http"
-	"os"
-	"time"
-	"user-service/initializers"
 	"user-service/internal/models"
-
+	"user-service/internal/database"
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func SignUp(c *gin.Context) {
-	// Get data off req body
 	var body struct{
 		Username string
 		Email 	 string
 		Password string
 	}
 
-	if c.Bind(&body) != nil{
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "failed to read body",
-		})
-
+	if Bind(c, &body) != 0 {
 		return
 	}
 	
-	// Hash de password
-	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10) // default cost
-
-	if err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "failed to hash password",
-		})
-
+	var hash string
+	if HashPassword(c, &hash, body.Password) != 0 {
 		return
 	}
 
-	// Create the user
-	user := models.User{Username: body.Username, Email: body.Email, Password: string(hash)}
-	result := initializers.DB.Create(&user)
-
-	if result.Error != nil{
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "failed to create user",
-		})
-
+	var user models.User = models.User{Username: body.Username, Email: body.Email, Password: string(hash)}
+	if database.CreateUser(c, user) != 0 {
 		return
 	}
 
-	// Respond
 	c.JSON(http.StatusOK, gin.H{})
 }
 
 func LogIn(c *gin.Context){
-	// Get the email and pass off req body
 	var body struct{
 		Email 	 string
 		Password string
 	}
-
-	if c.Bind(&body) != nil{
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "failed to read body",
-		})
-
+	
+	if Bind(c, &body) != 0 {
 		return
 	}
-	// Look up requested user
+	
 	var user models.User
-	initializers.DB.First(&user, "email = ?", body.Email)
-
-	if user.ID == 0{
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Invalid email",
-		})
-
-		return
-	}
-	// Compare sent in pass with saved user pass hash
-	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
-
-	if err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Invalid password",
-		})
-
-		return
-	}
-	// Generate jwt token
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"sub": user.ID,
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
-	})
-
-	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
-
-	if err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Failed to create token",
-		})
-
+	if database.CheckExistencebyEmail(c,&user, body.Email) != 0{
 		return
 	}
 
-	// Cookie
-	c.SetSameSite(http.SameSiteLaxMode)
-	c.SetCookie("Authorization", tokenString, 3600*24, "", "", false, true)
+	if CheckPassword(c, user, body.Password) != 0{
+		return
+	}
+	
+	var tokenString string
+	if GenerateToken(c, user, &tokenString) != 0{
+		return
+	}
+	
+	SetCookie(c, tokenString)
 
 	c.JSON(http.StatusOK, gin.H{})
 }
--- user-service/internal/database/user_db.go
+++ user-service/internal/database/user_db.go
@@ -1,1 +1,35 @@
 package database
+
+import (
+	"net/http"
+	"user-service/initializers"
+	"user-service/internal/models"
+
+	"github.com/gin-gonic/gin"
+)
+func CreateUser[T any](c *gin.Context, user T) int {
+
+	result := initializers.DB.Create(&user)
+
+	if result.Error != nil{
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "failed to create user",
+		})
+
+		return 1
+	}
+	return 0
+}
+
+func CheckExistencebyEmail(c *gin.Context, user *models.User, email string) int {
+	initializers.DB.First(&user, "email = ?", email)
+
+	if user.ID == 0{
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid email",
+		})
+
+		return 1
+	}
+	return 0
+}
